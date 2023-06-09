980f7d6950fc1c599e1f38f4c0ff10a93449847e
Kushal1306 <kalakushal.jain@gmail.com>
2023-06-09 17:34:09 +0530

    Setting matrix to zero in SC - O(1)

--- Arrays/set-zero2.cpp
+++ Arrays/set-zero2.cpp
@@ -1,29 +1,40 @@
-#include <bits/stdc++.h> 
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
-	// Write your code here.
-	int col[m]={0};
-	int row[n]={0};
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<m;j++)
-		{
-			if(matrix[i][j]==0)
-			{
-				row[i]=1;
-				col[j]=1;
-			}
-		}
-	}
+void setZeroes(vector<vector<int>>& matrix) {
+        int col=1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    if(j!=0)
+                    matrix[0][j]=0;
+                    else
+                    col=0;
 
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<m;j++)
-		{
-			if(row[i]||col[j])
-			{
-				matrix[i][j]=0;
-			}
-		}
-	}
-	return matrix;
-}
+                }
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][0]==0||matrix[0][j]==0)
+                matrix[i][j]=0;
+            }
+        }
+        if(matrix[0][0]==0)
+        {
+            for(int j=0;j<m;j++)
+            matrix[0][j]=0;
+        }
+        if(col==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+    }
