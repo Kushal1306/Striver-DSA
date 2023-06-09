43397544169c821b67063dd1b6334edf38e807ca
Kushal1306 <kalakushal.jain@gmail.com>
2023-06-09 16:48:55 +0530

    strivers sde sheet challenge

---
+++ Arrays/set-zero.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+	// Write your code here.
+	int col[m]={0};
+	int row[n]={0};
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(matrix[i][j]==0)
+			{
+				row[i]=1;
+				col[j]=1;
+			}
+		}
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(row[i]||col[j])
+			{
+				matrix[i][j]=0;
+			}
+		}
+	}
+	return matrix;
+}
